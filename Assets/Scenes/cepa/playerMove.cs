4ed9f285a44ffe1d5f60e6525a925a7f0fae3d6b
HansYang222 <hans.yang@vis.tp.edu.tw>
2025-05-08 15:32:03 +0800

    0.0.5 update

--- Assets/Scenes/cepa/playerMove.cs
+++ Assets/Scenes/cepa/playerMove.cs
@@ -4,24 +4,26 @@ public class playerMove : MonoBehaviour
 
 {
     public bool characterCanMove;
+    public float speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         characterCanMove = true;
+        speed = 0.01f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.D) && characterCanMove == true && gameObject.transform.localPosition.x < 110.0f)
+        if(Input.GetKey(KeyCode.D) && characterCanMove == true && gameObject.transform.localPosition.x < 2.88f)
         {
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x + 1f,gameObject.transform.position.y,gameObject.transform.position.z);
+            gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x + speed,gameObject.transform.localPosition.y, gameObject.transform.localPosition.z);
             //anim.SetBool("isWalking", true);
             //spriteRenderer.flipX = false;
         }
-        else if(Input.GetKey(KeyCode.A) && characterCanMove == true && gameObject.transform.localPosition.x > -70.6)
+        else if(Input.GetKey(KeyCode.A) && characterCanMove == true && gameObject.transform.localPosition.x > -2.88f)
         {
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x - 1f,gameObject.transform.position.y,gameObject.transform.position.z);
+            gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x - speed,gameObject.transform.localPosition.y, gameObject.transform.localPosition.z);
             //anim.SetBool("isWalking", true);
             //spriteRenderer.flipX = true;
         }
